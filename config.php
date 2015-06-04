19d32a9626fda3cc092f53b49c6a4e4597ba6721
Souq.com <api-support@souq.com>
2015-06-04 15:26:34 +0300

    Update Config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 Const CLIENT_ID = '';
 Const CLIENT_SECRET = '';
-Const REDIRECT_URL = 'http://stg.api.souq.com/souq-api-php/oauthExample.php';
+Const REDIRECT_URL = '';
 
 Const OAUTH_SCOPES = 'customer_profile,cart_management,customer_demographics';
 Const OAUTH_ACCESS_TOKEN = '';
